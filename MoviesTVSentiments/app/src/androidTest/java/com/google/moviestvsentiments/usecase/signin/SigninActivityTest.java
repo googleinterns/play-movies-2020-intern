2d44f76ac2a1d7deca26219f79925b56b906eecc
Jack Stenglein <jstenglein@google.com>
2020-06-19 12:36:31 -0500

    Add new accounts to the Room database

--- MoviesTVSentiments/app/src/androidTest/java/com/google/moviestvsentiments/usecase/signin/SigninActivityTest.java
+++ MoviesTVSentiments/app/src/androidTest/java/com/google/moviestvsentiments/usecase/signin/SigninActivityTest.java
@@ -4,10 +4,15 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.Intents.intending;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.google.moviestvsentiments.assertions.RecyclerViewItemCountAssertion.withItemCount;
 
+import android.app.Activity;
+import android.app.Instrumentation.ActivityResult;
+import android.content.Intent;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
@@ -29,6 +34,7 @@ public class SigninActivityTest {
 
     @Test
     public void signinActivity_displaysOnlyAddAccount() {
+        onView(withId(R.id.accountList)).check(withItemCount(1));
         onView(withId(R.id.accountTextView)).check(matches(withText("Add Account")));
     }
 
@@ -37,5 +43,28 @@ public class SigninActivityTest {
         onView(withId(R.id.accountTextView)).perform(click());
 
         intended(hasComponent(AddAccountActivity.class.getName()));
+    }
+
+    @Test
+    public void signinActivity_addAccountResultCanceled_displaysOnlyAddAccount() {
+        ActivityResult result = new ActivityResult(Activity.RESULT_CANCELED, new Intent());
+        intending(hasComponent(AddAccountActivity.class.getName())).respondWith(result);
+
+        onView(withId(R.id.accountTextView)).perform(click());
+
+        onView(withId(R.id.accountList)).check(withItemCount(1));
+        onView(withId(R.id.accountTextView)).check(matches(withText("Add Account")));
+    }
+
+    @Test
+    public void signinActivity_addAccountResultOk_displaysNewAccount() {
+        Intent intent = new Intent();
+        intent.putExtra(AddAccountActivity.EXTRA_ACCOUNT_NAME, "Account Name");
+        ActivityResult result = new ActivityResult(Activity.RESULT_OK, intent);
+        intending(hasComponent(AddAccountActivity.class.getName())).respondWith(result);
+
+        onView(withId(R.id.accountTextView)).perform(click());
+
+        onView(withId(R.id.accountList)).check(withItemCount(2));
     }
 }
