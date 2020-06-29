e59eaa378a535d46c132da756144c71a1febf349
Jack Stenglein <jstenglein@google.com>
2020-06-29 13:53:11 -0500

    Add basic design of DetailsActivity

---
+++ MoviesTVSentiments/app/src/main/java/com/google/moviestvsentiments/usecase/details/DetailsActivity.java
@@ -0,0 +1,14 @@
+package com.google.moviestvsentiments.usecase.details;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.google.moviestvsentiments.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
