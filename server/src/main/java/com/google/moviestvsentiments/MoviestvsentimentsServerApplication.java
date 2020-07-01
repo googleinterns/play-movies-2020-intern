3da5a36e67b964ba6ea9e91fc9504562e424d7c3
Jack Stenglein <jstenglein@google.com>
2020-07-01 14:07:48 -0500

    Add default server generated by Spring Initializr

---
+++ server/src/main/java/com/google/moviestvsentiments/MoviestvsentimentsServerApplication.java
@@ -0,0 +1,13 @@
+package com.google.moviestvsentiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviestvsentimentsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviestvsentimentsServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/google/moviestvsentiments/MoviestvsentimentsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.moviestvsentiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviestvsentimentsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
