baa28d735b6204b8641d0d29b22e92dab14f3f36
Jack Stenglein <jstenglein@google.com>
2020-07-23 16:20:08 -0500

    Add functionality to hamburger menu

---
+++ MoviesTVSentiments/app/src/androidTest/java/com/google/moviestvsentiments/usecase/navigation/SentimentsNavigationActivityTest.java
@@ -0,0 +1,99 @@
+package com.google.moviestvsentiments.usecase.navigation;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static com.google.common.truth.Truth.assertThat;
+
+import android.content.Intent;
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.intent.Intents;
+import com.google.moviestvsentiments.R;
+import com.google.moviestvsentiments.di.DatabaseModule;
+import com.google.moviestvsentiments.di.WebModule;
+import com.google.moviestvsentiments.model.Account;
+import com.google.moviestvsentiments.service.account.AccountDao;
+import com.google.moviestvsentiments.usecase.signin.SigninActivity;
+import com.google.moviestvsentiments.util.LiveDataTestUtil;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import java.time.Instant;
+import javax.inject.Inject;
+import dagger.hilt.android.testing.HiltAndroidRule;
+import dagger.hilt.android.testing.HiltAndroidTest;
+import dagger.hilt.android.testing.UninstallModules;
+
+@UninstallModules({DatabaseModule.class, WebModule.class})
+@HiltAndroidTest
+public class SentimentsNavigationActivityTest {
+
+    private static final String ACCOUNT_NAME = "Account Name";
+
+    private HiltAndroidRule hiltRule = new HiltAndroidRule(this);
+
+    @Rule
+    public RuleChain ruleChain = RuleChain.outerRule(hiltRule).around(new InstantTaskExecutorRule());
+
+    @Inject
+    AccountDao accountDao;
+
+    @Before
+    public void setUp() {
+        hiltRule.inject();
+        accountDao.addAccount(ACCOUNT_NAME, Instant.EPOCH, false);
+        accountDao.setIsCurrent(ACCOUNT_NAME, true);
+
+        Intents.init();
+
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(),
+                SentimentsNavigationActivity.class);
+        intent.putExtra(SigninActivity.EXTRA_ACCOUNT_NAME, ACCOUNT_NAME);
+        ActivityScenario.launch(intent);
+    }
+
+    @After
+    public void tearDown() {
+        Intents.release();
+    }
+
+    @Test
+    public void hamburgerMenu_bindsCorrectly() {
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+
+        onView(withId(R.id.hamburgerAccountName)).check(matches(withText(ACCOUNT_NAME)));
+        onView(withId(R.id.hamburgerAccountIconText)).check(matches(
+                withText(ACCOUNT_NAME.substring(0, 1))));
+    }
+
+    @Test
+    public void hamburgerMenu_selectSignOut_clearsCurrentAccount() {
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+
+        onView(withText(ApplicationProvider.getApplicationContext()
+                .getString(R.string.signoutLabel))).perform(click());
+
+        Account account = LiveDataTestUtil.getValue(accountDao.getCurrentAccount());
+        assertThat(account).isNull();
+    }
+
+    @Test
+    public void hamburgerMenu_selectSignOut_sendsIntentToSigninActivity() {
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+
+        onView(withText(ApplicationProvider.getApplicationContext()
+                .getString(R.string.signoutLabel))).perform(click());
+
+        intended(hasComponent(SigninActivity.class.getName()));
+    }
+}
--- MoviesTVSentiments/app/src/main/java/com/google/moviestvsentiments/usecase/navigation/SentimentsNavigationActivity.java
+++ MoviesTVSentiments/app/src/main/java/com/google/moviestvsentiments/usecase/navigation/SentimentsNavigationActivity.java
@@ -1,40 +1,91 @@
 package com.google.moviestvsentiments.usecase.navigation;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.moviestvsentiments.R;
+import com.google.moviestvsentiments.service.account.AccountViewModel;
+import com.google.moviestvsentiments.usecase.signin.SigninActivity;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import javax.inject.Inject;
 import dagger.hilt.android.AndroidEntryPoint;
 
 /**
  * An activity that manages switching between the Home, Liked and Disliked tabs.
  */
 @AndroidEntryPoint
-public class SentimentsNavigationActivity extends AppCompatActivity {
+public class SentimentsNavigationActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener {
+
+    @Inject
+    AccountViewModel viewModel;
+
+    private String accountName;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sentiments_navigation);
+
+        accountName = getIntent().getStringExtra(SigninActivity.EXTRA_ACCOUNT_NAME);
+        bindHamburgerMenu();
+        setupNavigationUI();
+
+        viewModel.getCurrentAccount().observe(this, account -> {
+            if (account == null) {
+                Intent intent = new Intent(this, SigninActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    /**
+     * Sets up the hamburger menu so that it displays the correct account name and will call this
+     * Activity to handle clicks on menu items.
+     */
+    private void bindHamburgerMenu() {
+        NavigationView hamburgerNav = findViewById(R.id.hamburger_nav);
+        hamburgerNav.setNavigationItemSelectedListener(this);
+
+        TextView accountNameView = hamburgerNav.getHeaderView(0)
+                .findViewById(R.id.hamburgerAccountName);
+        accountNameView.setText(accountName);
+
+        TextView accountIconInitial = hamburgerNav.getHeaderView(0)
+                .findViewById(R.id.hamburgerAccountIconText);
+        accountIconInitial.setText(accountName.substring(0, 1).toUpperCase());
+    }
+
+    /**
+     * Sets up the user interface so that it will display the bottom navigation view and the
+     * action bar.
+     */
+    private void setupNavigationUI() {
         BottomNavigationView navView = findViewById(R.id.nav_view);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawerLayout = findViewById(R.id.container);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
-                R.string.open_drawer, R.string.close_drawer);
+        drawerLayout = findViewById(R.id.container);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer,
+                R.string.close_drawer);
         drawerLayout.addDrawerListener(toggle);
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_liked, R.id.navigation_disliked)
                 .setDrawerLayout(drawerLayout)
@@ -42,5 +93,21 @@ public class SentimentsNavigationActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        toggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        if (menuItem.getItemId() == R.id.signout) {
+            viewModel.setIsCurrent(accountName, false);
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        }
+        return false;
     }
 }
