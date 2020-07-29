b99c9e15ad405713859528605a912bfe36676841
Jack Stenglein <jstenglein@google.com>
2020-07-29 10:38:24 -0500

    Improve UI when there are no assets

--- MoviesTVSentiments/app/src/main/java/com/google/moviestvsentiments/usecase/assetList/AssetListScreen.java
+++ MoviesTVSentiments/app/src/main/java/com/google/moviestvsentiments/usecase/assetList/AssetListScreen.java
@@ -2,6 +2,7 @@ package com.google.moviestvsentiments.usecase.assetList;
 
 import android.content.Context;
 import android.view.View;
+import android.widget.TextView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.google.moviestvsentiments.R;
@@ -15,6 +16,11 @@ public class AssetListScreen {
 
     private AssetListAdapter movieAdapter;
     private AssetListAdapter showAdapter;
+    private RecyclerView movieList;
+    private RecyclerView showList;
+    private TextView movieLabel;
+    private TextView showLabel;
+    private TextView noAssetsLabel;
 
     private AssetListScreen(AssetListAdapter.AssetClickListener assetClickListener) {
         movieAdapter = AssetListAdapter.create(assetClickListener);
@@ -37,12 +43,16 @@ public class AssetListScreen {
     }
 
     private void setupLists(View root, Context context) {
-        RecyclerView movieList = root.findViewById(R.id.movies_list);
+        noAssetsLabel = root.findViewById(R.id.no_assets_label);
+
+        movieLabel = root.findViewById(R.id.movies_label);
+        movieList = root.findViewById(R.id.movies_list);
         movieList.setAdapter(movieAdapter);
         movieList.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,
                 false));
 
-        RecyclerView showList = root.findViewById(R.id.tvshows_list);
+        showLabel = root.findViewById(R.id.tvshows_label);
+        showList = root.findViewById(R.id.tvshows_list);
         showList.setAdapter(showAdapter);
         showList.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,
                 false));
@@ -53,7 +63,7 @@ public class AssetListScreen {
      * @param movies The list of movies to display.
      */
     public void setMovies(List<AssetSentiment> movies) {
-        movieAdapter.setAssetSentiments(movies);
+        bind(movies, movieAdapter, movieList, movieLabel, showAdapter);
     }
 
     /**
@@ -61,6 +71,30 @@ public class AssetListScreen {
      * @param shows The list of TV shows to display.
      */
     public void setShows(List<AssetSentiment> shows) {
-        showAdapter.setAssetSentiments(shows);
+        bind(shows, showAdapter, showList, showLabel, movieAdapter);
+    }
+
+    /**
+     * Updates the user interface of the AssetListScreen for a new list of AssetSentiments.
+     * @param assetSentiments The new list of AssetSentiments that triggered the update.
+     * @param adapter The adapter that should display the list of AssetSentiments.
+     * @param list The RecyclerView that displays the adapter.
+     * @param label The label associated with the RecyclerView.
+     * @param otherAdapter The adapter associated with the list for the other type of Asset.
+     */
+    private void bind(List<AssetSentiment> assetSentiments, AssetListAdapter adapter,
+                      RecyclerView list, TextView label, AssetListAdapter otherAdapter) {
+        if (assetSentiments.isEmpty()) {
+            list.setVisibility(View.GONE);
+            label.setVisibility(View.GONE);
+            if (otherAdapter.getItemCount() == 0) {
+                noAssetsLabel.setVisibility(View.VISIBLE);
+            }
+        } else {
+            adapter.setAssetSentiments(assetSentiments);
+            list.setVisibility(View.VISIBLE);
+            label.setVisibility(View.VISIBLE);
+            noAssetsLabel.setVisibility(View.GONE);
+        }
     }
 }
